7aecf6f0405aa60f6cefe8db083ac9a2499dd6af
Sunab <sunabbaskota@gmail.com>
2025-12-02 22:20:46 +0545

    update the call

--- frontend-web/src/pages/Messages.jsx
+++ frontend-web/src/pages/Messages.jsx
@@ -380,11 +380,11 @@ const Messages = () => {
             </div>
 
             {/* Chat Area */}
-            <div className="col-span-12 lg:col-span-8 flex flex-col">
+            <div className="col-span-12 lg:col-span-8 flex flex-col h-full">
               {selectedChat ? (
                 <>
-                  {/* Chat Header */}
-                  <div className="p-4 border-b border-gray-200 bg-white">
+                  {/* Chat Header - Sticky at top */}
+                  <div className="flex-shrink-0 p-4 border-b border-gray-200 bg-white sticky top-0 z-10">
                     <div className="flex items-center justify-between">
                       <div className="flex items-center space-x-3">
                         <div className="relative flex-shrink-0">
@@ -457,8 +457,11 @@ const Messages = () => {
                     </div>
                   </div>
 
-                  {/* Messages */}
-                  <div className="flex-1 overflow-y-auto p-4 space-y-4 bg-gray-50">
+                  {/* Messages - Scrollable area */}
+                  <div
+                    className="flex-1 overflow-y-auto p-4 space-y-4 bg-gray-50"
+                    style={{ maxHeight: "calc(100vh - 20rem)" }}
+                  >
                     {messagesLoading ? (
                       <div className="flex items-center justify-center h-full">
                         <Loader className="w-8 h-8 text-blue-600 animate-spin" />
@@ -512,8 +515,8 @@ const Messages = () => {
                     <div ref={messagesEndRef} />
                   </div>
 
-                  {/* Message Input */}
-                  <div className="p-4 border-t border-gray-200 bg-white">
+                  {/* Message Input - Fixed at bottom */}
+                  <div className="flex-shrink-0 p-4 border-t border-gray-200 bg-white">
                     <form
                       onSubmit={handleSendMessage}
                       className="flex items-center space-x-2"
